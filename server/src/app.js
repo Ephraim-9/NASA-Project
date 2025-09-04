e4f5fac2625e029b876e82169ef0ee0bc4119c68
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-09-04 23:29:28 +0500

    ci: server frontend in prod

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 
@@ -11,6 +12,12 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(planetsRouter);
+
+app.get("/", (res, req) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
