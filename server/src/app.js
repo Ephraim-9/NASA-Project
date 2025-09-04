aeb15d961ab15ef9e1678dc3eed30d14a013e866
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-09-04 23:48:26 +0500

    fix: server script

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const planetsRouter = require("./routes/planets/planets.router");
 
@@ -11,12 +12,15 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+
+// app.use(morgan("combined"));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use(planetsRouter);
 
-app.get("/", (res, req) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
