4598e672160a18509b09c386cc95f013456fc44a
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-09-09 00:06:28 +0500

    fix: call getAllLaunches function

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,7 +1,7 @@
 const { getAllLaunches, addNewLaunch } = require("../../models/launches.model");
 
 function httpGetAllLaunches(req, res) {
-  res.status(200).json(getAllLaunches);
+  res.status(200).json(getAllLaunches());
 }
 
 function httpAddNewLaunch(req, res) {
