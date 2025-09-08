39a113d017f0ab6835d6403fee6ed88c6c47a605
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-09-08 23:21:39 +0500

    fix: client side routing error (replace "*" with "/{*any}")

--- server/src/app.js
+++ server/src/app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, "..", "public")));
 app.use("/planets", planetsRouter);
 app.use("/launches", launchesRouter);
 
-app.get("/", (req, res) => {
+app.get("/{*any}", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
