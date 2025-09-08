59dd346b184f3a232206d7bc06f76b314dc81c4f
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-09-08 11:29:56 +0500

    feat: POST launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,14 +1,16 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: "kepler Exploration X",
   rocket: "Explorer IS1",
   launchDate: new Date("December 27, 2030"),
   destination: "kepler-442 b",
-  coustomer: ["NASA"],
+  customer: ["NASA"],
   upcoming: true,
-  succes: true,
+  success: true,
 };
 
 launches.set(launch.flightNumber, launch);
@@ -17,6 +19,18 @@ function getAllLaunches() {
   return Array.from(launches.values());
 }
 
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      customers: ["NASA"],
+      flightNumber: latestFlightNumber,
+    })
+  );
+}
+
 module.exports = {
   getAllLaunches,
+  addNewLaunch,
 };
