7fe1aa5648ce48dc051847945d1d7cb82f8ac80e
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-09-10 13:42:47 +0500

    connect to mongodb

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,6 @@
 const http = require("http");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const app = require("./app");
 
@@ -6,9 +8,25 @@ const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = process.env.MONGO_URL;
+
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection is ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
   await loadPlanetsData();
 
   server.listen(PORT, () => {
