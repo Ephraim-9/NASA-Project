80c8c993827fc6a21813e1f3ff13eaae939e833f
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-09-10 13:54:46 +0500

    feat: mongoose schema for launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -8,7 +8,7 @@ const launch = {
   rocket: "Explorer IS1",
   launchDate: new Date("December 27, 2030"),
   target: "kepler-442 b",
-  customer: ["NASA"],
+  customers: ["NASA"],
   upcoming: true,
   success: true,
 };
@@ -30,7 +30,7 @@ function addNewLaunch(launch) {
     Object.assign(launch, {
       success: true,
       upcoming: true,
-      customer: ["NASA"],
+      customers: ["NASA"],
       flightNumber: latestFlightNumber,
     })
   );
