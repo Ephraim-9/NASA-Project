af066556c8cd9e7963eb641b8eec1eb4e7e12f62
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-09-12 19:34:56 +0500

    feat: persist SpaceX launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -43,6 +43,11 @@ async function populateLaunches() {
     },
   });
 
+  if (response.status !== 200) {
+    console.log("problem downloading launch data");
+    throw new Error("launch data download failed");
+  }
+
   const launchDocs = response.data.docs;
   for (const launchDoc of launchDocs) {
     const payloads = launchDoc["payloads"];
@@ -60,6 +65,8 @@ async function populateLaunches() {
       customers,
     };
     console.log(`${launch.flightNumber} ${launch.mission}`);
+
+    await saveLaunch(launch);
   }
 }
 
@@ -99,13 +106,6 @@ async function getAllLaunches() {
 }
 
 async function saveLaunch(launch) {
-  const planet = await planets.findOne({
-    keplerName: launch.target,
-  });
-
-  if (!planet) {
-    throw new Error("no matching planet found");
-  }
   await launches.findOneAndUpdate(
     {
       flightNumber: launch.flightNumber,
@@ -118,6 +118,14 @@ async function saveLaunch(launch) {
 }
 
 async function scheduleNewLaunch(launch) {
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+
+  if (!planet) {
+    throw new Error("no matching planet found");
+  }
+
   const newFlightNumber = (await getLatestFlightNumber()) + 1;
 
   const newLaunch = Object.assign(launch, {
